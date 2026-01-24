322668d2260255c8e648cbde9c76362e59e7442c
Inês Monteiro <inesmonteirosara@gmail.com>
2026-01-24 20:24:48 +0000

    rm librarys from ft_tolower and ft_toupper

--- functions/ft_tolower.c
+++ functions/ft_tolower.c
@@ -1,5 +1,3 @@
-#include "libft.h"
-
 int ft_tolower(int character)
 {
     if ((character >= 'A') && (character <= 'Z'))
--- functions/ft_toupper.c
+++ functions/ft_toupper.c
@@ -1,5 +1,3 @@
-#include "libft.h"
-
 int ft_toupper(int character)
 {
     if ((character >= 'a') && (character <= 'z'))
